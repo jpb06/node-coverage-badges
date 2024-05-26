c449653dd7eb713f3b89c90d988b27aadffd10b3
jpb06 <jp.bois.06@outlook.fr>
2024-05-26 15:04:33 +0200

    chore: exporting effect (#2)

--- src/logic/generation/generateBadges.logic.ts
+++ src/logic/generation/generateBadges.logic.ts
@@ -7,25 +7,30 @@ import { emptyDir, ensureDir, readJson } from '@logic/effects/fsExtra.effects';
 
 import { generateCoverageFile } from './generateCoverageFile.logic';
 
+export const generateBadgesEffect = (
+  coverageSummaryPath = defaultSummaryPath,
+  outputPath = defaultOutputDir,
+  logo = defaultIcon,
+) =>
+  pipe(
+    Effect.all([ensureDir(outputPath), emptyDir(outputPath)]),
+    Effect.flatMap(() =>
+      readJson<CoverageSummaryFileContent>(coverageSummaryPath),
+    ),
+    Effect.flatMap((summary) =>
+      Effect.all(
+        coverageKeysArray.map(generateCoverageFile(summary, outputPath, logo)),
+        { concurrency: 'unbounded' },
+      ),
+    ),
+    Effect.map(() => true),
+  );
+
 export const generateBadges = async (
   coverageSummaryPath = defaultSummaryPath,
   outputPath = defaultOutputDir,
   logo = defaultIcon,
 ) =>
   Effect.runPromise(
-    pipe(
-      Effect.all([ensureDir(outputPath), emptyDir(outputPath)]),
-      Effect.flatMap(() =>
-        readJson<CoverageSummaryFileContent>(coverageSummaryPath),
-      ),
-      Effect.flatMap((summary) =>
-        Effect.all(
-          coverageKeysArray.map(
-            generateCoverageFile(summary, outputPath, logo),
-          ),
-          { concurrency: 'unbounded' },
-        ),
-      ),
-      Effect.map(() => true),
-    ),
+    generateBadgesEffect(coverageSummaryPath, outputPath, logo),
   );
