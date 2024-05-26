e5f6d47ae673e39d9aa3ea123304d392c13a4b40
jpb06 <jp.bois.06@outlook.fr>
2024-05-26 15:11:00 +0200

    fix: missing export in index (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 /* istanbul ignore file */
 import { CoverageSummary, FileCoverageTotal } from '@types';
 
-import { generateBadges } from '@logic/generation/generateBadges.logic';
+import {
+  generateBadges,
+  generateBadgesEffect,
+} from '@logic/generation/generateBadges.logic';
 
-export { generateBadges };
+export { generateBadges, generateBadgesEffect };
 export type { CoverageSummary, FileCoverageTotal };
